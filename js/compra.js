e617b0ee190efd5ad085fdcf4f4f4c1fbd356b0f
JuanPabloCM <65985353+JuanPabloCM@users.noreply.github.com>
2020-12-15 14:53:34 -0600

    Update compra.js

--- js/compra.js
+++ js/compra.js
@@ -11,35 +11,42 @@ myFunction(valor);
      case "1":
        document.frmcompra.f1t1.value = "cara de papa";
        document.frmcompra.f1t2.value = 250.00;
-       document.getElementById("fiu").style.backgroundImage = "url('https://i.pinimg.com/564x/d2/18/bd/d218bd994932215dcb9e006c92eca90b.jpg')";
+       document.getElementById("fiu").style.backgroundImage = "url('https://i.pinimg.com/564x/c0/96/30/c09630d1ce8ff6107875a3d15344eb8b.jpg')";
        break;
      case "2":
        document.frmcompra.f1t1.value = "corona virus";
        document.frmcompra.f1t2.value = 275.00;
+       document.getElementById("fiu").style.backgroundImage = "url('https://i.pinimg.com/564x/d2/18/bd/d218bd994932215dcb9e006c92eca90b.jpg')";
        break;
      case "3":
        document.frmcompra.f1t1.value = "cubetazo";
        document.frmcompra.f1t2.value = 265.00;
+       document.getElementById("fiu").style.backgroundImage = "url('https://i.pinimg.com/564x/63/9c/aa/639caa71b0e5f8fb05488cf26c75d1a5.jpg')";
        break;
      case "4":
        document.frmcompra.f1t1.value = "pavo real";
        document.frmcompra.f1t2.value = 280.00;
+     document.getElementById("fiu").style.backgroundImage = "url('https://i.pinimg.com/564x/b1/d3/d4/b1d3d48553d7f35f81ad5d9e92d95d86.jpg')";
        break;
      case "5":
        document.frmcompra.f1t1.value = "alebrije";
        document.frmcompra.f1t2.value = 275.00;
+       document.getElementById("fiu").style.backgroundImage = "url('https://i.pinimg.com/564x/f1/f8/9a/f1f89a48ad952a3ca9852020cb59bc0a.jpg')";
        break;
      case "6":
        document.frmcompra.f1t1.value = "piñata navideña";
        document.frmcompra.f1t2.value = 255.00;
+       document.getElementById("fiu").style.backgroundImage = "url('https://i.pinimg.com/564x/d1/0d/ba/d10dba9a380c865d9c387cec5c94286f.jpg')";
        break;
      case "7":
        document.frmcompra.f1t1.value = "saquen el esposo";
        document.frmcompra.f1t2.value = 290.00;
+       document.getElementById("fiu").style.backgroundImage = "url('https://image.mmradio.com/sites/default/files/styles/amp_metadata_content_image_min_696px_wide/public/pinata_1.jpg')";
        break;
      case "8":
        document.frmcompra.f1t1.value = "mujer de vida galante";
        document.frmcompra.f1t2.value = 300.00;
+       document.getElementById("fiu").style.backgroundImage = "url('https://i.pinimg.com/564x/cc/af/52/ccaf52d2fee15df103cba6ecefef480e.jpg')";
        break;
      default:
        alert("No existe esa piñata");
